c4d6c728f310a955c6f7ac73ac7083c01407ebf3
VICTOR AMWOLLO <amwollo@yahoo.com>
2023-05-14 22:21:44 +0300

    add manager store validation

---
+++ app/Http/Requests/ManagerRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ManagerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    // public function authorize(): bool
+    // {
+    //     return false;
+    // }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'supermarket_id' => 'required|unique:managers',
+            'phone' => 'required|unique:managers',
+            'email' => 'required|unique:managers',
+        ];
+
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Name is required',
+            'phone.required' => 'Phone is required',
+            'supermarket_id.required' => 'Supermarket is required',
+            'email.required' => 'Email is required'
+        ];
+
+    }
+}
