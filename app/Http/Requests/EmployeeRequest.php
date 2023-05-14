25e07a432c7efe0ddb0d70f293e053c254cf0b5b
VICTOR AMWOLLO <amwollo@yahoo.com>
2023-05-14 23:06:38 +0300

    add validation rules

---
+++ app/Http/Requests/EmployeeRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class EmployeeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    // public function authorize(): bool
+    // {
+    //     return false;
+    // }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'phone' => 'required|max:12|min:10|unique:employees',
+            'supermarket_id' => 'required',
+            'type' => 'required',
+            'manager_id' => 'required',
+            'gender' => 'required',
+        ];
+
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Name is required',
+            'type.required' => 'Employee type is required',
+            'gender.required' => 'Gender is required',
+            'supermarket_id.required' => 'Supermarket is required',
+            'manager_id.required' => 'Manager is required',
+            'phone.required' => 'Phone is required'
+        ];
+
+    }
+}
