016744e8539fb974e00aa91c2246697ef5266d1a
VICTOR AMWOLLO <amwollo@yahoo.com>
2023-05-15 06:00:54 +0300

    add bulk upload file validation rules

---
+++ app/Http/Requests/BulkUploadRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class BulkUploadRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    // public function authorize(): bool
+    // {
+    //     return false;
+    // }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules()
+    {
+        return [
+            'file' => 'required|mimes:csv|max:2048',
+            'user_id' => 'required',
+        ];
+
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+
+    }
+
+    public function messages()
+    {
+        return [
+            'file.required' => 'File is required',
+            'user_id.required' => 'User is required',
+        ];
+    }
+}
