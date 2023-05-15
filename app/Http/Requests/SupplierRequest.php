d219a9d6f24648367c76ddc47255ec1648205462
VICTOR AMWOLLO <amwollo@yahoo.com>
2023-05-15 05:47:41 +0300

    add validation rules

--- app/Http/Requests/SupplierRequest.php
+++ app/Http/Requests/SupplierRequest.php
@@ -2,27 +2,54 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class SupplierRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return false;
-    }
+    // public function authorize(): bool
+    // {
+    //     return false;
+    // }
 
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+            'name' => 'required|max:255',
+            'supermarket_id' => 'required',
+            'phone' => 'required|unique:suppliers',
+            'location' => 'required',
         ];
+
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Supplier name is required',
+            'phone.required' => 'Phone is required',
+            'supermarket_id.required' => 'Supermarket is required',
+            'location.required' => 'Location is required'
+        ];
+
     }
 }
